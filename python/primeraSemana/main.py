067201e06c3a17a10af7803427e4acc081823f14
Diego Rodriguez Vargas <nanofer2@gmail.com>
2024-08-13 17:55:15 -0500

    agrego ejercicios de python

---
+++ python/primeraSemana/main.py
@@ -0,0 +1,10 @@
+print("Hola mundo")
+
+nombre ="Diego Rodriguez"
+edad = 30
+
+print ("Hola "+nombre+" Tienes", edad ," Aňos")
+
+nombre2 = input("Digite el nombre: ")
+
+print(f"Hola , {nombre2}!")
