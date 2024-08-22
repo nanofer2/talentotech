52dce32b44fbea4c7140680aab65ec6cc077851b
diego rodriguez vargas <nanofer2@gmail.com>
2024-08-22 17:52:32 -0500

    Se agrega ejemplo metodo get

---
+++ Cymetria/Http/appexample.js
@@ -0,0 +1,60 @@
+// paso final - creacion de elementos
+
+const listElement = document.querySelector(".posts");
+const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("#posts-container");
+
+// estructura de peticion requuest
+
+function sendHTTPRequest(method, url, data) {
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => {
+    return response.json();
+  });
+}
+
+//request get
+
+async function fetchPost() {
+  const responseData = await sendHTTPRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  console.log(responseData);
+  const listOfPost = responseData;
+
+  for (const post of listOfPost) {
+    const postContainer = document.createElement("article");
+    postContainer.id = post.id;
+    postContainer.classList.add("post-item");
+
+    const title = document.createElement("h2");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    const button = document.createElement("button");
+    button.textContent = "DELETE Content";
+
+    postContainer.append(title);
+    postContainer.append(body);
+    postContainer.append(button);
+
+    listElement.append(postContainer);
+  }
+}
+
+// fetchPost();
+
+fetchButton.addEventListener("click", fetchPost);
+
+// hasta aqui fue el primer paso que fue crear la funcion y la peticion, ahora toca extraer la informacion
+// iteramos sobre el arreglo de objetos
